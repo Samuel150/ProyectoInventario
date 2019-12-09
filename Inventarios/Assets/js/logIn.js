bd2032788c9b869b3b7c3d7f102b9450c6d3bf1f
Samuel <hsamueljackson4@gmail.com>
2019-12-09 07:47:16 -0400

    Usando Ajax

--- Inventarios/Assets/js/logIn.js
+++ Inventarios/Assets/js/logIn.js
@@ -1,6 +1,21 @@
 
-function logIn(){
+var button = document.getElementById("buttonLogIn");
+
+button.addEventListener("click", function(){
+    var request = new XMLHttpRequest();
     var user = document.getElementById("user").value;
     var password = document.getElementById("password").value;
-    console.log(user,password);
-}
+    request.open("GET","http://skynet.lp.upb.edu:7875/usuarios", true);
+    //request.withCredentials = true;
+    //request.setRequestHeader("Usuario", "/");
+    request.onload = function () {
+        console.log(request.responseText);
+    };
+    request.send();
+
+});
+
+
+//coockies , sesiones
+
+
