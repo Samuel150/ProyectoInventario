578f04bb809c523d29658d69666de1aa3ac5297c
Samuel <hsamueljackson4@gmail.com>
2019-12-11 09:14:12 -0400

    Autorizacion y Autenticacion

--- Inventarios/Templates/logIn.php
+++ Inventarios/Templates/logIn.php
@@ -1,7 +1,12 @@
 <?php
-
-    $ci = $_POST[CI];
-    $tipo = $_POST[Tipo];
     session_start();
+    session_destroy();
+    session_start();
+
+    $ci = $_POST['CI'];
+    $tipo = $_POST['Tipo'];
     $_SESSION['CI'] = $ci;
     $_SESSION['TIPO'] = $tipo;
+    //exit(json_encode(array("msg"=>$_SESSION['CI'].$_SESSION['TIPO'])));
+
+    ?>
