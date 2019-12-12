649e01ea93bb7899d33d586b4e40f851188b9fe7
Rodrixkill <31656922+Rodrixkill@users.noreply.github.com>
2019-12-12 15:03:29 -0400

    name to session

--- Inventarios/Templates/logIn.php
+++ Inventarios/Templates/logIn.php
@@ -5,8 +5,10 @@
 
     $ci = $_POST['CI'];
     $tipo = $_POST['Tipo'];
+    $nombre = $_POST['Nombre'];
     $_SESSION['CI'] = $ci;
     $_SESSION['TIPO'] = $tipo;
+    $_SESSION['NOMBRE']= $nombre;
     //exit(json_encode(array("msg"=>$_SESSION['CI'].$_SESSION['TIPO'])));
 
     ?>
