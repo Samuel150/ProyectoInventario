ce810ed520814a536b010c343b92b80c758ef9ba
Samuel <hsamueljackson4@gmail.com>
2019-12-12 21:07:47 -0400

    Encriptacion

--- Inventarios/Templates/logIn.php
+++ Inventarios/Templates/logIn.php
@@ -1,12 +1,16 @@
 <?php
     session_start();
     session_destroy();
-    session_start();
 
+    session_start();
+    session_set_cookie_params(0);
     $ci = $_POST['CI'];
+    $password = $_POST['Contrasena'];
     $tipo = $_POST['Tipo'];
+
     $_SESSION['CI'] = $ci;
     $_SESSION['TIPO'] = $tipo;
-    //exit(json_encode(array("msg"=>$_SESSION['CI'].$_SESSION['TIPO'])));
+    $_SESSION['CONTRASENA'] = $password;
+    exit(json_encode(array("msg"=>$_SESSION['CI'].$_SESSION['TIPO'].$_SESSION['CONTRASENA'])));
 
-    ?>
+?>
