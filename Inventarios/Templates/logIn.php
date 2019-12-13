5aa50cef595c97648c392f9a9bc1a0101408b499
Rodrixkill <rodrigo.villarroel.m@gmail.com>
2019-12-13 08:11:10 -0400

    Inventarios

--- Inventarios/Templates/logIn.php
+++ Inventarios/Templates/logIn.php
@@ -7,11 +7,14 @@
     $ci = $_POST['CI'];
     $password = $_POST['Contrasena'];
     $tipo = $_POST['Tipo'];
+    $nombre= $_POST['Nombre'];
+
 
 
     $_SESSION['CI'] = $ci;
     $_SESSION['TIPO'] = $tipo;
     $_SESSION['CONTRASENA'] = $password;
+    $_SESSION['NOMBRE']= $nombre;
     exit(json_encode(array("msg"=>$_SESSION['CI'].$_SESSION['TIPO'].$_SESSION['CONTRASENA'])));
 
 
